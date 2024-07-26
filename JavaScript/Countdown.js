1424b4b5ee58876377cf154eb3dd8b7c4d8d939a
iyiinsan5555 <mhbsonmez@gmail.com>
2024-07-26 20:56:32 +0300

    Updated

--- JavaScript/Countdown.js
+++ JavaScript/Countdown.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const targetDate = new Date('June 22, 2025');
+    const OkultargetDate = new Date('September 9, 2024');
     const countdownElement = document.getElementById('countdown');
-    const animationElement = document.getElementById('animationElement');
+    const okulcountdownElement = document.getElementById('okulcountdown');
 
     function updateCountdown() {
         const now = new Date();
@@ -15,5 +16,18 @@ document.addEventListener('DOMContentLoaded', function() {
         countdownElement.innerHTML = `${days} gün ${hours} saat ${minutes} dakika ${seconds} saniye`;
     }
 
+    function updateOkulCountdown() {
+        const now = new Date();
+        const difference = OkultargetDate - now;
+
+        let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+        okulcountdownElement.innerHTML ="Okulların Açılmasına: " + `${days} gün ${hours} saat ${minutes} dakika ${seconds} saniye`;
+    }
+
     setInterval(updateCountdown, 1000);
+    setInterval(updateOkulCountdown, 1000);
 });
