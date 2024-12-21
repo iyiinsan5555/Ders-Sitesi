18fda22206169b74498d12c7529ceb8316a34ca9
iyiinsan5555 <mhbsonmez@gmail.com>
2024-12-21 16:51:00 +0300

    Updated

---
+++ JavaScript/YoutubeMatematikAkıllıKampı.js
@@ -0,0 +1,34 @@
+// Get today's date
+const today = new Date();
+const startDate = new Date('December 21, 2024');
+
+// Calculate the difference in milliseconds
+const differenceInMilliseconds = Math.abs(today - startDate);
+console.log(Math.abs(today - startDate));
+
+// Convert milliseconds to days
+const differenceInDays = Math.floor(differenceInMilliseconds / (1000 * 60 * 60 * 24));
+
+// Define the period
+const Period = 4;
+
+// Create a function to calculate the remaining days
+function getRemainingDays(days) {
+    return days % Period;
+}
+
+// Calculate the remaining days
+const PeriodikDay = getRemainingDays(differenceInDays);
+console.log("Toplam Geçen Gün: ", differenceInDays)
+console.log("Periyodik Gün: ", PeriodikDay);
+
+const selectedElementId = PeriodikDay + ".Gün"
+const FoundElement = document.getElementById(selectedElementId);
+
+FoundElement.classList.remove("notSelected")
+FoundElement.classList.add("Selected");
+
+
+document.getElementById("StartDay").textContent = " Başlangıç Günü: 21 Aralık 2024"
+document.getElementById("Today").textContent = "Bugün: "+today.toLocaleDateString('tr-TR', { year: 'numeric', month: 'long', day: 'numeric' });
+document.getElementById("DifferenceInDays").textContent = "Geçen Gün: "+differenceInDays
