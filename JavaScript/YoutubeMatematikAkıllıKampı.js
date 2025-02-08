0e887eb095fbee79adde0cb34629f83591c62f85
iyiinsan5555 <mhbsonmez@gmail.com>
2025-02-08 11:42:56 +0300

    Kanal Eklendi

--- JavaScript/YoutubeMatematikAkıllıKampı.js
+++ JavaScript/YoutubeMatematikAkıllıKampı.js
@@ -10,7 +10,7 @@ console.log(Math.abs(today - startDate));
 const differenceInDays = Math.floor(differenceInMilliseconds / (1000 * 60 * 60 * 24));
 
 // Define the period
-const Period = 7;
+const Period = 8;
 
 // Create a function to calculate the remaining days
 function getRemainingDays(days) {
